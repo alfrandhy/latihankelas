06607ed36334d0c038751b4af4f0f2480f7e42ae
Alfrandhy <alfrandhy@gmail.com>
2020-11-26 09:05:13 +0700

    edit category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -25,4 +25,36 @@ class CategoryController extends Controller
         Category::create($request->except('_token'));
         return redirect(route('category'))->with(['success' => 'Category Ditambah']);
     }
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        $parent = Category::getParent()->orderBy('name','ASC')->get();
+        return view('admin.categories.edit', compact('category','parent'));
+    }
+    public function Update(Request $request, $id)
+    {
+        $this->validate($request,[
+            'name'=>'required|string|max:50|unique:categories,name,' .$id
+        ]);
+        $category = Category::find($id);
+        $category->update([
+            'name'=>$request->name,
+            'parent_id'=>$request->parent_id,
+            'slug' => $request->name
+        ]);
+        return redirect(route('category.index'))->with(['success' => 'Category Diperbaharui']);
+    }
+    public function destroy($id)
+    {
+        // $category=Category::withCount(['child','product'])->find($id);
+        // if ($category->child_count == 0 && $category->product_count == 0) {
+        //     $category->delete();
+        //     return redirect(route('category.index'))->with(['success' => 'Category Dihapus']);
+        // }
+        // return redirect(route('category.index'))->with(['error' => 'Hapus Sub Category Terlebih Dahulu']);
+        $category = Category::find($id)->delete();
+        return redirect(Route('category'));
+    }
+
+
 }
